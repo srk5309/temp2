25a14e0c4b642ebe8e379356409cd67acd39ab1c
srk5309 <53816542+srk5309@users.noreply.github.com>
2021-06-02 21:24:26 +0530

    Add files via upload

---
+++ Referral.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Referral = ()=> {
+    return (
+        <View>
+            <Text>Referral</Text>
+        </View>
+    )
+}
+
+export default Referral
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
